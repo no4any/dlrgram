c02be794ea2be3b7498526a4ad65b751ec50cd2e
Martin Karry <martin@karry.de>
2023-02-21 11:09:21 +0100

    Basic setup of next-auth

---
+++ packages/dlrgram/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth"
+import { AuthOptions } from "next-auth/core/types"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+export const authOptions:AuthOptions = {
+  providers: [
+    CredentialsProvider({
+        name: "Credentials",
+        credentials: {
+          username: { label: "Username", type: "text", placeholder: "Usrename" },
+          password: { label: "Password", type: "password" }
+        },
+        async authorize(credentials, req) {
+          const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+    
+          if (user) {
+            return user
+          } else {
+            return null
+          }
+        }
+      })
+  ],
+}
+
+export default NextAuth(authOptions)
