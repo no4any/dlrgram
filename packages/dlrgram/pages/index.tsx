208f276e3e8ccc218aeee6fe6dea04ba216dd87e
Martin Karry <martin@karry.de>
2023-02-21 13:44:00 +0100

    Path update

--- packages/dlrgram/pages/index.tsx
+++ packages/dlrgram/pages/index.tsx
@@ -2,8 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import SessionTest from '@/components/SessionTest.component'
-import ApiTest from '@/components/ApiTest'
+import SessionTest from '@/src/components/SessionTest.component'
+import ApiTest from '@/src/components/ApiTest'
 
 const inter = Inter({ subsets: ['latin'] })
 
