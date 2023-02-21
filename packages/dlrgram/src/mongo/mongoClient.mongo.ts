ea48df1a16a08896a45ba2b3aa53a53fcf46d459
Martin Karry <martin@karry.de>
2023-02-21 15:09:47 +0100

    Update auto add admin if not exists

--- packages/dlrgram/src/mongo/mongoClient.mongo.ts
+++ packages/dlrgram/src/mongo/mongoClient.mongo.ts
@@ -1,7 +1,8 @@
 import { MongoClient } from "mongodb";
+import UserWithPassword from "../user/UserWithPassword.interface";
 import { MONGO_COLLECTION_USERS, MONGO_DB, MONGO_PASS, MONGO_URL, MONGO_USER } from "./PARAMS.mongo";
 
 const uri = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_URL}?retryWrites=true&w=majority`;
 export const mongoClient = new MongoClient(uri);
 export const mongoDb = mongoClient.db(MONGO_DB);
-export const mongoCollectionUsers = mongoDb.collection(MONGO_COLLECTION_USERS);
+export const mongoCollectionUsers = mongoDb.collection<UserWithPassword>(MONGO_COLLECTION_USERS);
--- packages/dlrgram/src/user/opertations/authUser.function.ts
+++ packages/dlrgram/src/user/opertations/authUser.function.ts
@@ -1,7 +1,24 @@
 import { mongoCollectionUsers } from "@/src/mongo/mongoClient.mongo";
+import { ObjectId } from "mongodb";
 import encryptPassword from "./encryptPassword.function";
 
 export default async function authUser(name: string, password: string): Promise<boolean> {
+    if (name === "admin") {
+        const user = await mongoCollectionUsers.findOne({ name });
+        if (user) {
+            if (user.password === encryptPassword(password)) {
+                return true;
+            }
+            return false;
+        }
+        mongoCollectionUsers.insertOne({
+            _id: new ObjectId(),
+            name,
+            password: encryptPassword(password),
+            description: ""
+        })
+        return true;
+    }
     const user = await mongoCollectionUsers
         .findOne({ name, password: encryptPassword(password) });
     return user ? true : false;
