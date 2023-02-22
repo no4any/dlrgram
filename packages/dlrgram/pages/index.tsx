9695fa37b06f26b3a98da9b5d63ba5e48efca0ad
Martin Karry <martin@karry.de>
2023-02-22 22:03:27 +0100

    Added fontawsome

--- packages/dlrgram/pages/index.tsx
+++ packages/dlrgram/pages/index.tsx
@@ -4,11 +4,14 @@ import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import SessionTest from '@/src/components/SessionTest.component'
 import ApiTest from '@/src/components/ApiTest'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSadCry } from '@fortawesome/free-regular-svg-icons'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return <>
+    <h1>Testseite <FontAwesomeIcon icon={faSadCry} /></h1>
     <SessionTest />
     <ApiTest />
   </>
